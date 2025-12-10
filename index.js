878b5e7ada5b6dbb97d1586f2c2f04161edd1d1e
Bani Banerjee <banibanerjee09@gmail.com>
2025-12-10 20:42:26 +0530

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({
-  origin:['http://localhost:3000',
+  origin:[
   'https://my-product-curd-frontend.vercel.app/'
 
 ] ,
